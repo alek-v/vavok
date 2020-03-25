fbb33f761a3d35fc57ea4220918da00fdfa3bf10
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2020-03-25 16:26:19 +0100

    Update index.php

--- themes/default/index.php
+++ themes/default/index.php
@@ -4,51 +4,50 @@
 // custom page tempates directory for this theme
 // if you want to use custom template dir
 // set here template dir for this theme
+// dir must be under template main folder
 $themeCustomTpl = '';
 
 // get page title
-if (!isset($my_title)) {
-    $my_title = page_title($phpself);
-}
-// if we still dont have a page title
-if (empty($my_title)) {
-	$my_title = $config["title"];
-}
+if (!isset($my_title)) { $my_title = page_title($phpself); }
+
+// if we still dont have a page title use website main title
+if (empty($my_title)) { $my_title = $config["title"]; }
 
 // load head tags for specific page
-// if we are not installing script
+$head_tag = '';
+// if we are not installing cms
 if (!stristr($phpself, 'install/install.php')) {
-    // check is this user made page
+    // is this regular page
     if (!empty($pg)) {
         $vk_page = $db->select('pages', "pname='" . $pg . "'", '', 'headt');
-        $head_tag = $vk_page['headt'];
+        $head_tag .= $vk_page['headt'];
     } 
-    // data not found, contnue...
+    // if data not found, contnue...
+    // this is used if this is custom page
     if (empty($head_tag)) {
         $vk_page = $db->select('pages', "pname='" . $clean_requri . "'", '', 'headt');
         if (!empty($vk_page['headt'])) {
-            $head_tag = $vk_page['headt'];
+            $head_tag .= $vk_page['headt'];
         } 
     } 
     // no data using $clean_requri, try PHP_SELF :)
     if (empty($head_tag)) {
         $vk_page = $db->select('pages', "pname='" . $phpself . "'", '', 'headt');
         if (!empty($vk_page['headt'])) {
-            $head_tag = $vk_page['headt'];
+            $head_tag .= $vk_page['headt'];
         } 
     } 
-} 
-// append system generated head tags
-if (empty($head_tag) && !empty($genHeadTag)) {
-    $head_tag = $genHeadTag;
-} 
-// if $head_tag is already set
-elseif (!empty($head_tag) && !empty($genHeadTag)) {
-    $head_tag .= $genHeadTag;
-} 
+}
+
+// append head tags for specific pages
+if (!empty($genHeadTag)) { $head_tag .= $genHeadTag; }
+
+// tell bots what is our preferred page
+if (stristr($head_tag, 'rel="canonical"') === false && isset($pg)) { $head_tag .= "\n" . '<link rel="canonical" href="' . $connectionProtocol . $config_srvhost . '/' . $pg . '" />'; }
+
+
 // header
 header("Content-type:text/html; charset=utf-8");
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -58,24 +57,16 @@ header("Content-type:text/html; charset=utf-8");
 <?php
 // head tags <head></head>
 // head tags for current theme
-echo '<link rel="stylesheet" type="text/css" href="' . HOMEDIR . 'themes/default/style.css?v=' . filemtime(BASEDIR . 'themes/default/style.css') . '" />
-';
+echo '<link rel="stylesheet" type="text/css" href="' . HOMEDIR . 'themes/default/style.css?v=' . filemtime(BASEDIR . 'themes/default/style.css') . '" />';
 
 // include custom (user made) <head> tags for all pages
 echo file_get_contents(BASEDIR . 'used/headmeta.dat');
 
 // include head tags specified for current page
-if (!empty($head_tag)) {
-    echo $head_tag;
-}
+if (!empty($head_tag)) { echo $head_tag; }
 
 // cookie consent
-if ($config['cookieConsent'] == 1) {
-    include_once BASEDIR . "include/plugins/cookie-consent/cookie-consent.php";
-}
-
-// tell bots what is our preferred page
-echo '<link rel="canonical" href="' . $connectionProtocol . $config_srvhost . $clean_requri . '" />';
+if ($config['cookieConsent'] == 1) { include_once BASEDIR . "include/plugins/cookie-consent/cookie-consent.php"; }
 
 echo "\r\n<!-- Vavok CMS http://www.vavok.net -->
 <title>" . $my_title . "</title>
@@ -84,9 +75,7 @@ echo "\r\n<!-- Vavok CMS http://www.vavok.net -->
 
 // site body
 
-echo '
-<div id="wrapper">
-';
+echo '<div id="wrapper">';
 
 echo '<header><div class="c"><strong>' . $config["title"] . '</strong></div></header>';
 
