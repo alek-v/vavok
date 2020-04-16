c6a9dc63c885acd539eb3ecf9ca0dd13494c34a0
Aleksandar Vranešević <bg.aleksandar@gmail.com>
2020-04-16 21:07:09 +0200

    Update add missing open graph tags

--- include/prepare_header.php
+++ include/prepare_header.php
@@ -1,6 +1,6 @@
 <?php
 // (c) Aleksandar Vranešević - https://vavok.net
-// modified: 10.04.2020. 19:53:17
+// modified: 16.04.2020. 20:41:05
 // prepare headers for page
 
 // get page title
@@ -14,34 +14,59 @@ $head_tag = '';
 
 // if we are not installing cms
 if (!stristr($phpself, 'install/install.php')) {
+
     // is this regular page
     if (!empty($pg)) {
-        $vk_page = $db->select('pages', "pname='" . $pg . "'", '', 'headt');
+
+        $vk_page = $db->get_data('pages', "pname='" . $pg . "' AND lang='" . $ln_loc . "'");
         $head_tag .= $vk_page['headt'];
-    } 
-    // if data not found, contnue...
-    // this is used if this is custom page
-    if (empty($head_tag)) {
-        $vk_page = $db->select('pages', "pname='" . $clean_requri . "'", '', 'headt');
-        if (!empty($vk_page['headt'])) {
+
+        if (empty($head_tag)) {
+            $vk_page = $db->get_data('pages', "pname='" . $pg . "'");
             $head_tag .= $vk_page['headt'];
-        } 
+        }
+
     } 
+
     // no data using $clean_requri, try PHP_SELF :)
     if (empty($head_tag)) {
-        $vk_page = $db->select('pages', "pname='" . $phpself . "'", '', 'headt');
+        $vk_page = $db->get_data('pages', "pname='" . $phpself . "'");
         if (!empty($vk_page['headt'])) {
             $head_tag .= $vk_page['headt'];
         } 
     } 
+
+
+    // if it is index page
+    if (stristr($_SERVER['PHP_SELF'], '/index.php') && empty($head_tag)) {
+
+        // first we check is there a page with language we use
+        $vk_page = $db->get_data('pages', "pname='index' AND lang='" . $ln_loc . "'");
+
+        if (empty($vk_page['headt'])) {
+            // load default index page title
+            $vk_page = $db->get_data('pages', "pname='index'");
+            $head_tag .= $vk_page['headt'];
+
+        }
+
+    }
+
 }
 
 // append head tags for specific pages
 if (!empty($genHeadTag)) { $head_tag .= $genHeadTag; }
 
-// tell bots what is our preferred page
-if (stristr($head_tag, 'rel="canonical"') === false && isset($pg)) { $head_tag .= "\n" . '<link rel="canonical" href="' . $connectionProtocol . $config_srvhost . '/page/' . $pg . '/" />'; }
 
+// check for missing tags
+
+// tell bots what is our preferred page
+if (!stristr($head_tag, 'rel="canonical"') && isset($pg)) { $head_tag .= "\n" . '<link rel="canonical" href="' . $connectionProtocol . $config_srvhost . '/page/' . $pg . '/" />'; }
+
+// add missing open graph tags
+if (!strstr($head_tag, 'og:type')) { $head_tag .= "\n" . '<meta property="og:type" content="website" />'; }
+
+if (!strstr($head_tag, 'og:title')) { $head_tag .= "\n" . '<meta property="og:title" content="' . $vk_page['tname'] . '" />'; }
 
 
 
