c3007ae798f011893fc782660a327d757d914cfb
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2020-06-05 21:32:43 +0200

    Update use new API

--- adminpanel/ip-informations.php
+++ adminpanel/ip-informations.php
@@ -3,70 +3,54 @@
 require_once"../include/strtup.php";
 
 if (!$users->is_reg() || (!$users->is_moderator() && !$users->is_administrator())) {
-header("Location: ../input.php?action=exit");
-exit;
+    redirect_to("../pages/input.php?action=exit");
 }
 
 $ip = check($_GET['ip']);
 
 if (empty($ip)) { exit('please set ip address'); }
 
-$my_title = 'IP Informations';
-include'../themes/' . $config_themes . '/index.php';
-
 // Get an array with geoip-infodata
-function geoCheckIP($ip) { 
+function geo_check_ip($ip) {
+
     // check, if the provided ip is valid
     if (!filter_var($ip, FILTER_VALIDATE_IP)) {
         throw new InvalidArgumentException("IP is not valid");
     } 
+
     // contact ip-server
-    $response = @file_get_contents('http://www.netip.de/search?query=' . $ip);
+    $response = @file_get_contents('http://ip-api.com/json/' . $ip);
+
     if (empty($response)) {
         throw new InvalidArgumentException("Error contacting Geo-IP-Server");
     } 
-    // Array containing all regex-patterns necessary to extract ip-geoinfo from page
-    $patterns = array();
-    $patterns["domain"] = '#Domain: (.*?)&nbsp;#i';
-    $patterns["country"] = '#Country: (.*?)&nbsp;#i';
-    $patterns["state"] = '#State/Region: (.*?)<br#i';
-    $patterns["town"] = '#City: (.*?)<br#i'; 
-    // Array where results will be stored
-    $ipInfo = array(); 
-    // check response from ipserver for above patterns
-    foreach ($patterns as $key => $pattern) {
-        // store the result in array
-        $ipInfo[$key] = preg_match($pattern, $response, $value) && !empty($value[1]) ? $value[1] : 'not found';
-    } 
 
-    return $ipInfo;
+    // Return result as array
+    return json_decode($response, true);
+
 } 
 
+$ipData = geo_check_ip($ip);
 
-// Array ( [domain] => dslb-094-219-040-096.pools.arcor-ip.net [country] => DE - Germany [state] => Hessen [town] => Erzhausen )
-// Array ( [domain] => not found [country] => DE - Germany [state] => Hessen [town] => Heppenheim )
-$ipData = geoCheckIP($ip);
+$my_title = 'IP Informations';
+include'../themes/' . $config_themes . '/index.php';
 
 ?>
 
-<h2>Informations about IP</h2>
+<h1>Informations about IP</h1>
+
 <div class="b">
 
 <?php
+
 echo 'IP Address: ' . $ip . '<br />';
 echo 'Country: ' . $ipData['country'] . '<br />';
-echo 'State/Region: ' . $ipData['state'] . '<br />';
-echo 'City/Town: ' . $ipData['town'] . '<br />';
-echo 'Domain: ' . $ipData['domain'] . '<br />';
+echo 'State/Region: ' . $ipData['regionName'] . '<br />';
+echo 'City/Town: ' . $ipData['city'] . '<br />';
 echo '</div>';
-?>
-<div class="clear"></div>
-<div class="break"></div>
 
-<?php
-echo '<a href="./" class="btn btn-outline-primary sitelink">' . $lang_home['admpanel'] . '</a><br>';
-echo '<a href="../" class="btn btn-primary homepage">' . $lang_home['home'] . '</a><br>';
-
+echo '<p><a href="./" class="btn btn-outline-primary sitelink">' . $lang_home['admpanel'] . '</a><br>';
+echo '<a href="../" class="btn btn-primary homepage">' . $lang_home['home'] . '</a></p>';
 
 include'../themes/' . $config_themes . '/foot.php';
 
