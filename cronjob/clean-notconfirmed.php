f4b7e1bd791d22c4f0d28eff0670a00dbbdc2811
alek-v <bg.aleksandar@gmail.com>
2020-07-25 22:15:41 +0200

    Updated cronjob

--- cronjob/clean-notconfirmed.php
+++ cronjob/clean-notconfirmed.php
@@ -1,22 +1,21 @@
 <?php
-// modified: 26.3.2015. 2:09:28
+// modified: 25.07.2020. 21:20:45
 // (c) vavok.net
 
 include"../include/startup.php";
 //include"/home/vavoknet/public_html/include/startup.php";
 
 // hours user have to confirm registration
-$confirmationHours = 48;
+$confirmationHours = 24;
 
 $confirmationTime = $confirmationHours * 60 * 60;
 
 foreach ($db->query("SELECT `regdate`, `uid` FROM `vavok_profil` WHERE `regche`='1'") as $userCheck) {
 	if (($userCheck['regdate'] + $confirmationTime) < time()) {
-// delete user if he didn't confirmed registration within $confirmationHours
-delete_users($userCheck['uid']);
+		// delete user if he didn't confirmed registration within $confirmationHours
+		$users->delete_user($userCheck['uid']);
+	}
 }
-}
-exit;
 
 // this cron job will delete users if they didn't complete registration within $confirmationHours
 // php /home/username/public_html/cronjob/clean-notconfirmed.php >/dev/null - this should be your php script path and command to run cron job
