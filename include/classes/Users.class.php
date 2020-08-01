4b6242787f2e8c4b208be1cbe2702d19411b4547
alek-v <bg.aleksandar@gmail.com>
2020-08-01 22:17:18 +0200

    Updated cookie management

--- include/classes/Users.class.php
+++ include/classes/Users.class.php
@@ -4,22 +4,68 @@
 * Author:    Aleksandar Vranešević
 * URI:       https://vavok.net
 * Package:   Class for user management
-* Updated:   29.07.2020. 18:41:02
+* Updated:   01.08.2020. 19:10:55
 */
 
 
 class Users {
 
 	public $user_id;
+	public $vavok;
+	public $db;
 
 	function __construct() {
-		global $db;
+		global $db, $vavok;
+
+		$this->db = $db;
+		$this->vavok = $vavok;
 
 		// Session
 		session_name("sid");
 		session_start();
 
-		$this->db = $db;
+		/**
+		 * Set session from cookie data
+		 */
+		if (empty($_SESSION['log']) && !empty($_COOKIE['cookpass']) && !empty($_COOKIE['cooklog']) && !empty($this->vavok->get_configuration('keypass'))) {
+
+		    // decode username from cookie
+		    $unlog = $this->vavok->xoft_decode($_COOKIE['cooklog'], $this->vavok->get_configuration('keypass'));
+
+		    // decode password from cookie
+		    $unpar = $this->vavok->xoft_decode($_COOKIE['cookpass'], $this->vavok->get_configuration('keypass'));
+		    
+		    // search for username provided in cookie
+			$cookie_id = $this->getidfromnick($unlog);
+
+		    // get user's data
+			$cookie_check = $this->db->get_data('vavok_users', "id='" . $cookie_id . "'", 'name, pass, perm, lang');
+
+		    // if user exists
+		    if (!empty($cookie_check['name'])) {
+
+		        // check is password correct
+		        if ($this->password_check($unpar, $cookie_check['pass']) && $unlog == $cookie_check['name']) {
+
+		            $pr_ip = explode(".", $this->find_ip());
+		            $my_ip = $pr_ip[0] . $pr_ip[1] . $pr_ip[2];
+
+
+		            // write current session data
+		            $_SESSION['log'] = $unlog;
+		            $_SESSION['permissions'] = $cookie_check['perm'];
+		            $_SESSION['my_ip'] = $my_ip;
+		            $_SESSION['my_brow'] = $this->user_browser();
+		            $_SESSION['lang'] = $cookie_check['lang'];
+		            
+		            // update ip address and last visit time
+		            $db->update('vavok_users', 'ipadd', $this->find_ip(), "id = '{$cookie_id}'");
+		            $db->update('vavok_profil', 'lastvst', time(), "uid = '{$cookie_id}'");
+		        }
+		    } 
+		}
+
+		// User's id
 		$this->user_id = isset($_SESSION['log']) ? $this->getidfromnick($_SESSION['log']) : '';
 		
 		// Get user data
@@ -46,7 +92,7 @@ class Users {
 		    } 
 
 		    // Check if user is banned
-		    if ($vavok_users['banned'] == "1" && !strstr($_SERVER['PHP_SELF'], 'pages/ban.php')) redirect_to(BASEDIR . "pages/ban.php");
+		    if ($vavok_users['banned'] == "1" && !strstr($_SERVER['PHP_SELF'], 'pages/ban.php')) $vavok->redirect_to(BASEDIR . "pages/ban.php");
 
 		 	// activate account
 		    if ($user_profil['regche'] == 1 && !strstr($_SERVER['PHP_SELF'], 'pages/key.php')) {
@@ -62,7 +108,7 @@ class Users {
 		} else {
 
 			// User's site theme
-		    $config_themes = get_configuration('webtheme');
+		    $config_themes = $this->vavok->get_configuration('webtheme');
 
 		    if (empty($_SESSION['lang'])) $this->change_language();
 
@@ -91,7 +137,7 @@ class Users {
 		$this->visited_pages = $_SESSION['counton'];
 
 		// visitor's time on the site
-		$this->time_on_site = maketime(round(time() - $_SESSION['currs']));
+		$this->time_on_site = $this->vavok->maketime(round(time() - $_SESSION['currs']));
 
 		/*
 		User settings
@@ -101,13 +147,17 @@ class Users {
 		define("MY_THEME", $config_themes);
 
 		// If timezone is not defined use default
-		if (!defined('MY_TIMEZONE')) define('MY_TIMEZONE', get_configuration('timeZone'));
+		if (!defined('MY_TIMEZONE')) define('MY_TIMEZONE', $this->vavok->get_configuration('timeZone'));
 
 	}
 
-	// check current session if user is registered
-	public function is_reg() {
-
+	/**
+	 * Check if user is registered
+	 *
+	 * @return bool
+	 */
+	public function is_reg()
+	{
 	    if (!empty($_SESSION['log']) && !empty($_SESSION['permissions'])) {
 
 	        if (!empty($this->user_id)) {
@@ -115,7 +165,7 @@ class Users {
 	            $show_user = $this->db->get_data('vavok_users', "id='{$this->user_id}'", 'name, perm');
 
 	            // Check if permissions are changed
-	            if (check($_SESSION['log']) == $show_user['name'] && $_SESSION['permissions'] == $show_user['perm']) {
+	            if ($this->vavok->check($_SESSION['log']) == $show_user['name'] && $_SESSION['permissions'] == $show_user['perm']) {
 	                return true; // everything is ok
 	            } else {
 	            	$this->logout($this->user_id);
@@ -129,12 +179,16 @@ class Users {
 	    } else {
 	    	return false;
 	    }
-
 	}
 
-	// Logout
-	public function logout($user_id) {
-
+	/**
+	 * Logout
+	 *
+	 * @param integer $user_id
+	 * @return void
+	 */
+	public function logout($user_id)
+	{
 	    $this->db->delete('online', "user = '{$this->user_id}'");
 
 	    // destroy cookies
@@ -148,16 +202,16 @@ class Users {
 
 	    // destoy session
 	    session_destroy();
-
 	}
 
 	// count login attempts
-	public function login_attempt_count($seconds, $username, $ip, $db) {
+	public function login_attempt_count($seconds, $username, $ip)
+	{
 	    try {
 	        // First we delete old attempts from the table
 	        $oldest = strtotime(date("Y-m-d H:i:s") . " - " . $seconds . " seconds");
 	        $oldest = date("Y-m-d H:i:s", $oldest);
-	        $db->delete('login_attempts', "`datetime` < '" . $oldest . "'");
+	        $this->db->delete('login_attempts', "`datetime` < '" . $oldest . "'");
 	        
 	        // Next we insert this attempt into the table
 	        $values = array(
@@ -165,10 +219,10 @@ class Users {
 	        'datetime' =>  date("Y-m-d H:i:s"),
 	        'username' => $username
 	        );
-	        $db->insert_data('login_attempts', $values);
+	        $this->db->insert_data('login_attempts', $values);
 	        
 	        // Finally we count the number of recent attempts from this ip address  
-	        $attempts = $db->count_row('login_attempts', " `address` = '" . $_SERVER['REMOTE_ADDR'] . "' AND `username` = '" . $username . "'");
+	        $attempts = $this->db->count_row('login_attempts', " `address` = '" . $_SERVER['REMOTE_ADDR'] . "' AND `username` = '" . $username . "'");
 
 	        return $attempts;
 	    } catch (PDOEXCEPTION $e) {
@@ -177,28 +231,29 @@ class Users {
 	}
 
 	// register user
-	public function register($name, $pass, $regkeys, $rkey, $theme, $mail) {
-	    global $localization, $db, $vavok;
+	public function register($name, $pass, $regkeys, $rkey, $theme, $mail)
+	{
+	    global $localization;
 	    
 	    $values = array(
 	        'name' => $name,
 	        'pass' => $this->password_encrypt($pass),
 	        'perm' => '107',
 	        'skin' => $theme,
-	        'browsers' => check($this->user_browser()),
+	        'browsers' => $this->vavok->check($this->user_browser()),
 	        'ipadd' => $this->find_ip(),
 	        'timezone' => 0,
 	        'banned' => 0,
 	        'newmsg' => 0,
-	        'lang' => $vavok->get_configuration('siteDefaultLang')
+	        'lang' => $this->vavok->get_configuration('siteDefaultLang')
 	    );
-	    $db->insert_data('vavok_users', $values);
+	    $this->db->insert_data('vavok_users', $values);
 
-	    $user_id = $db->get_data('vavok_users', "name='{$name}'", 'id')['id'];
+	    $user_id = $this->db->get_data('vavok_users', "name='{$name}'", 'id')['id'];
 
-	    $db->insert_data('vavok_profil', array('uid' => $user_id, 'opentem' => 0, 'commadd' => 0, 'subscri' => 0, 'regdate' => time(), 'regche' => $regkeys, 'regkey' => $rkey, 'lastvst' => time(), 'forummes' => 0, 'chat' => 0));
-	    $db->insert_data('vavok_about', array('uid' => $user_id, 'sex' => 'N', 'email' => $mail));
-	    $db->insert_data('notif', array('uid' => $user_id, 'lstinb' => 0, 'type' => 'inbox'));
+	    $this->db->insert_data('vavok_profil', array('uid' => $user_id, 'opentem' => 0, 'commadd' => 0, 'subscri' => 0, 'regdate' => time(), 'regche' => $regkeys, 'regkey' => $rkey, 'lastvst' => time(), 'forummes' => 0, 'chat' => 0));
+	    $this->db->insert_data('vavok_about', array('uid' => $user_id, 'sex' => 'N', 'email' => $mail));
+	    $this->db->insert_data('notif', array('uid' => $user_id, 'lstinb' => 0, 'type' => 'inbox'));
 
 	    // send private message
 	    $msg = $localization->string('autopmreg');
@@ -206,19 +261,23 @@ class Users {
 
 	}
 
-	/*
+	/**
+	 * Update informations about user
+	 */
 
-	Update informations about user
-
-	*/
-
-	// Change user's language
-	public function change_language($language = '') {
+	/**
+	 * Change user's language
+	 *
+	 * @param string $language
+	 * @return void
+	 */
+	public function change_language($language = '')
+	{
 		$language = $this->get_prefered_language($language);
 		$current_session = isset($_SESSION['lang']) ? $_SESSION['lang'] : '';
 
-		// Update language if it is changed
-		if ($current_session != $language) {
+		// Update language if it is changed and if language is installed
+		if ($current_session != $language && file_exists(BASEDIR . 'include/lang/' . $language . '/index.php')) {
 			// unset current language
 			$_SESSION['lang'] = "";
 			unset($_SESSION['lang']);
@@ -229,12 +288,11 @@ class Users {
 			// Update language if user is registered
 			if ($this->is_reg()) { $this->db->update('vavok_users', 'lang', $language, "id='{$this->user_id}'"); }
 		}
-
 	}
 
 	// delete user from database
-	public function delete_user($users) {
-
+	public function delete_user($users)
+	{
 	    // check if it is really user's id
 	    if (preg_match("/^([0-9]+)$/", $users)) {
 	        $users_id = $users;
@@ -253,14 +311,11 @@ class Users {
 	    $this->db->delete("specperm", "uid = '{$users_id}'");
 
 	    return $users;
-
 	}
 
-	/*
-
-	Inbox
-
-	*/
+	/**
+	 * Inbox
+	 */
 
 	// private messages
 	public function getpmcount($uid, $view = "all") {
@@ -301,12 +356,11 @@ class Users {
 	} 
 
 	public function parsepm($text) {
-		
 		// decode
 		$text = base64_decode($text);
 
 		// format message
-	    $text = getbbcode(smiles(antiword($text)));
+	    $text = $this->vavok->getbbcode($this->vavok->smiles($this->vavok->antiword($text)));
 
 	    // strip slashes
 	    if (get_magic_quotes_gpc()) {
@@ -337,7 +391,7 @@ class Users {
             $user_mail = $this->db->get_data('vavok_about', "uid='{$who}'", 'email');
 
             $send_mail = new Mailer();
-            $send_mail->send($user_mail['email'], "Message on " . get_configuration('homeUrl'), "Hello " . $users->getnickfromid($who) . "\r\n\r\nYou have new message on site " . get_configuration('homeUrl')); // update lang
+            $send_mail->send($user_mail['email'], "Message on " . $this->vavok->get_configuration('homeUrl'), "Hello " . $users->getnickfromid($who) . "\r\n\r\nYou have new message on site " . $this->vavok->get_configuration('homeUrl')); // update lang
 
             $this->db->update('notif', 'lstinb', $time, "uid='" . $who . "' AND type='inbox'");
         }
@@ -453,7 +507,7 @@ class Users {
 			// Use language from session if exists
 			if (!empty($_SESSION['lang'])) { return $_SESSION['lang']; }
 			else {
-				return get_configuration('siteDefaultLang');
+				return $this->vavok->get_configuration('siteDefaultLang');
 			}
 		}
 	}
@@ -486,10 +540,21 @@ class Users {
 	    return $statwho;
 	}
 
+	// Administrator status name
+	function user_status($message) {
+	    global $localization;
+	    $message = str_replace('101', $localization->string('access101'), $message);
+	    $message = str_replace('102', $localization->string('access102'), $message);
+	    $message = str_replace('103', $localization->string('access103'), $message);
+	    $message = str_replace('105', $localization->string('access105'), $message);
+	    $message = str_replace('106', $localization->string('access106'), $message);
+	    $message = str_replace('107', $localization->string('access107'), $message);
+	    return $message;
+	}
+
 	// check permissions for admin panel
 	// check if user have permitions to see, edit, delete, etc selected part of the website
 	function check_permissions($permname, $needed = 'show') {
-
 	    $permname = str_replace('.php', '', $permname);
 
 	    if ($this->is_administrator()) {
@@ -543,7 +608,6 @@ class Users {
     	return BrowserDetection::userDevice();
 	}
 
-
 	// visitor's browser
 	function user_browser() {
 
@@ -558,11 +622,9 @@ class Users {
 		return $userBrowser;
 	}
 
-	/*
-
-	Validations
-
-	*/
+	/**
+	 * Validations
+	 */
 
 	// username validation
 	function validate_username($username) {
@@ -575,9 +637,8 @@ class Users {
 
 	// email validation with support for unicode emails
 	function validate_email($email) {
-
 		// check unicode email
-		if (is_unicode($email)) {
+		if ($this->vavok->is_unicode($email)) {
 
 		    if (preg_match("/^(?!\.)((?!.*\.{2})[a-zA-Z0-9\x{0080}-\x{00FF}\x{0100}-\x{017F}\x{0180}-\x{024F}\x{0250}-\x{02AF}\x{0300}-\x{036F}\x{0370}-\x{03FF}\x{0400}-\x{04FF}\x{0500}-\x{052F}\x{0530}-\x{058F}\x{0590}-\x{05FF}\x{0600}-\x{06FF}\x{0700}-\x{074F}\x{0750}-\x{077F}\x{0780}-\x{07BF}\x{07C0}-\x{07FF}\x{0900}-\x{097F}\x{0980}-\x{09FF}\x{0A00}-\x{0A7F}\x{0A80}-\x{0AFF}\x{0B00}-\x{0B7F}\x{0B80}-\x{0BFF}\x{0C00}-\x{0C7F}\x{0C80}-\x{0CFF}\x{0D00}-\x{0D7F}\x{0D80}-\x{0DFF}\x{0E00}-\x{0E7F}\x{0E80}-\x{0EFF}\x{0F00}-\x{0FFF}\x{1000}-\x{109F}\x{10A0}-\x{10FF}\x{1100}-\x{11FF}\x{1200}-\x{137F}\x{1380}-\x{139F}\x{13A0}-\x{13FF}\x{1400}-\x{167F}\x{1680}-\x{169F}\x{16A0}-\x{16FF}\x{1700}-\x{171F}\x{1720}-\x{173F}\x{1740}-\x{175F}\x{1760}-\x{177F}\x{1780}-\x{17FF}\x{1800}-\x{18AF}\x{1900}-\x{194F}\x{1950}-\x{197F}\x{1980}-\x{19DF}\x{19E0}-\x{19FF}\x{1A00}-\x{1A1F}\x{1B00}-\x{1B7F}\x{1D00}-\x{1D7F}\x{1D80}-\x{1DBF}\x{1DC0}-\x{1DFF}\x{1E00}-\x{1EFF}\x{1F00}-\x{1FFF}\x{20D0}-\x{20FF}\x{2100}-\x{214F}\x{2C00}-\x{2C5F}\x{2C60}-\x{2C7F}\x{2C80}-\x{2CFF}\x{2D00}-\x{2D2F}\x{2D30}-\x{2D7F}\x{2D80}-\x{2DDF}\x{2F00}-\x{2FDF}\x{2FF0}-\x{2FFF}\x{3040}-\x{309F}\x{30A0}-\x{30FF}\x{3100}-\x{312F}\x{3130}-\x{318F}\x{3190}-\x{319F}\x{31C0}-\x{31EF}\x{31F0}-\x{31FF}\x{3200}-\x{32FF}\x{3300}-\x{33FF}\x{3400}-\x{4DBF}\x{4DC0}-\x{4DFF}\x{4E00}-\x{9FFF}\x{A000}-\x{A48F}\x{A490}-\x{A4CF}\x{A700}-\x{A71F}\x{A800}-\x{A82F}\x{A840}-\x{A87F}\x{AC00}-\x{D7AF}\x{F900}-\x{FAFF}\.!#$%&'*+-\/=?^_`{|}~\-\d]+)@(?!\.)([a-zA-Z0-9\x{0080}-\x{00FF}\x{0100}-\x{017F}\x{0180}-\x{024F}\x{0250}-\x{02AF}\x{0300}-\x{036F}\x{0370}-\x{03FF}\x{0400}-\x{04FF}\x{0500}-\x{052F}\x{0530}-\x{058F}\x{0590}-\x{05FF}\x{0600}-\x{06FF}\x{0700}-\x{074F}\x{0750}-\x{077F}\x{0780}-\x{07BF}\x{07C0}-\x{07FF}\x{0900}-\x{097F}\x{0980}-\x{09FF}\x{0A00}-\x{0A7F}\x{0A80}-\x{0AFF}\x{0B00}-\x{0B7F}\x{0B80}-\x{0BFF}\x{0C00}-\x{0C7F}\x{0C80}-\x{0CFF}\x{0D00}-\x{0D7F}\x{0D80}-\x{0DFF}\x{0E00}-\x{0E7F}\x{0E80}-\x{0EFF}\x{0F00}-\x{0FFF}\x{1000}-\x{109F}\x{10A0}-\x{10FF}\x{1100}-\x{11FF}\x{1200}-\x{137F}\x{1380}-\x{139F}\x{13A0}-\x{13FF}\x{1400}-\x{167F}\x{1680}-\x{169F}\x{16A0}-\x{16FF}\x{1700}-\x{171F}\x{1720}-\x{173F}\x{1740}-\x{175F}\x{1760}-\x{177F}\x{1780}-\x{17FF}\x{1800}-\x{18AF}\x{1900}-\x{194F}\x{1950}-\x{197F}\x{1980}-\x{19DF}\x{19E0}-\x{19FF}\x{1A00}-\x{1A1F}\x{1B00}-\x{1B7F}\x{1D00}-\x{1D7F}\x{1D80}-\x{1DBF}\x{1DC0}-\x{1DFF}\x{1E00}-\x{1EFF}\x{1F00}-\x{1FFF}\x{20D0}-\x{20FF}\x{2100}-\x{214F}\x{2C00}-\x{2C5F}\x{2C60}-\x{2C7F}\x{2C80}-\x{2CFF}\x{2D00}-\x{2D2F}\x{2D30}-\x{2D7F}\x{2D80}-\x{2DDF}\x{2F00}-\x{2FDF}\x{2FF0}-\x{2FFF}\x{3040}-\x{309F}\x{30A0}-\x{30FF}\x{3100}-\x{312F}\x{3130}-\x{318F}\x{3190}-\x{319F}\x{31C0}-\x{31EF}\x{31F0}-\x{31FF}\x{3200}-\x{32FF}\x{3300}-\x{33FF}\x{3400}-\x{4DBF}\x{4DC0}-\x{4DFF}\x{4E00}-\x{9FFF}\x{A000}-\x{A48F}\x{A490}-\x{A4CF}\x{A700}-\x{A71F}\x{A800}-\x{A82F}\x{A840}-\x{A87F}\x{AC00}-\x{D7AF}\x{F900}-\x{FAFF}\-\.\d]+)((\.([a-zA-Z\x{0080}-\x{00FF}\x{0100}-\x{017F}\x{0180}-\x{024F}\x{0250}-\x{02AF}\x{0300}-\x{036F}\x{0370}-\x{03FF}\x{0400}-\x{04FF}\x{0500}-\x{052F}\x{0530}-\x{058F}\x{0590}-\x{05FF}\x{0600}-\x{06FF}\x{0700}-\x{074F}\x{0750}-\x{077F}\x{0780}-\x{07BF}\x{07C0}-\x{07FF}\x{0900}-\x{097F}\x{0980}-\x{09FF}\x{0A00}-\x{0A7F}\x{0A80}-\x{0AFF}\x{0B00}-\x{0B7F}\x{0B80}-\x{0BFF}\x{0C00}-\x{0C7F}\x{0C80}-\x{0CFF}\x{0D00}-\x{0D7F}\x{0D80}-\x{0DFF}\x{0E00}-\x{0E7F}\x{0E80}-\x{0EFF}\x{0F00}-\x{0FFF}\x{1000}-\x{109F}\x{10A0}-\x{10FF}\x{1100}-\x{11FF}\x{1200}-\x{137F}\x{1380}-\x{139F}\x{13A0}-\x{13FF}\x{1400}-\x{167F}\x{1680}-\x{169F}\x{16A0}-\x{16FF}\x{1700}-\x{171F}\x{1720}-\x{173F}\x{1740}-\x{175F}\x{1760}-\x{177F}\x{1780}-\x{17FF}\x{1800}-\x{18AF}\x{1900}-\x{194F}\x{1950}-\x{197F}\x{1980}-\x{19DF}\x{19E0}-\x{19FF}\x{1A00}-\x{1A1F}\x{1B00}-\x{1B7F}\x{1D00}-\x{1D7F}\x{1D80}-\x{1DBF}\x{1DC0}-\x{1DFF}\x{1E00}-\x{1EFF}\x{1F00}-\x{1FFF}\x{20D0}-\x{20FF}\x{2100}-\x{214F}\x{2C00}-\x{2C5F}\x{2C60}-\x{2C7F}\x{2C80}-\x{2CFF}\x{2D00}-\x{2D2F}\x{2D30}-\x{2D7F}\x{2D80}-\x{2DDF}\x{2F00}-\x{2FDF}\x{2FF0}-\x{2FFF}\x{3040}-\x{309F}\x{30A0}-\x{30FF}\x{3100}-\x{312F}\x{3130}-\x{318F}\x{3190}-\x{319F}\x{31C0}-\x{31EF}\x{31F0}-\x{31FF}\x{3200}-\x{32FF}\x{3300}-\x{33FF}\x{3400}-\x{4DBF}\x{4DC0}-\x{4DFF}\x{4E00}-\x{9FFF}\x{A000}-\x{A48F}\x{A490}-\x{A4CF}\x{A700}-\x{A71F}\x{A800}-\x{A82F}\x{A840}-\x{A87F}\x{AC00}-\x{D7AF}\x{F900}-\x{FAFF}]){2,63})+)$/u", $email)) {
 				return true; 
@@ -699,7 +760,7 @@ class Users {
 		$locale = isset($_SERVER["HTTP_ACCEPT_LANGUAGE"]) ? substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2) : '';
 
 		// Use default language
-		if (empty($language)) { $language = get_configuration('siteDefaultLang'); }
+		if (empty($language)) { $language = $this->vavok->get_configuration('siteDefaultLang'); }
 
 
 		if ($language == 'en') {
