b0e818579c1d78a4db9b706ab19db7723cd3374f
alek-v <bg.aleksandar@gmail.com>
2020-08-01 22:14:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,24 @@
 * (c) Aleksandar Vranešević
 * Author:    Aleksandar Vranešević
 * URI:       https://vavok.net
-* Updated:   24.07.2020. 16:08:36
+* Updated:   01.08.2020. 2:10:31
 */
 
 require_once "include/startup.php";
 
-// Page header
+/**
+ * Page header
+ */
 require_once BASEDIR . "themes/" . MY_THEME . "/index.php";
 
-// Show page content
+/**
+ * Page content
+ */
 $current_page->show_page();
 
-// Load website footer
+/**
+ * Page footer
+ */
 require_once BASEDIR . "themes/" . MY_THEME . "/foot.php";
 
 ?>
