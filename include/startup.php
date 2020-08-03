98d00faec5f4485a7ecb8ece62c7b0c290e81573
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2020-08-03 08:37:47 +0200

    Update startup.php

--- include/startup.php
+++ include/startup.php
@@ -3,11 +3,11 @@
 * (c) Aleksandar Vranešević
 * Author:    Aleksandar Vranešević
 * URI:       https://vavok.net
-* Updated:   02.08.2020. 18:00:00
+* Updated:   03.08.2020. 8:07:52
 */
 
 define('START_TIME', microtime(true));
-define('VERSION', '1.5.5.beta2');
+define('VERSION', '1.5.5.beta4');
 
 /**
  * Root dir for including system files
@@ -35,7 +35,7 @@ $vavok = new Vavok();
 /**
  * Connect to database
  */
-$db = new Db(DB_HOST, DB_DATABASE, DB_USERNAME, DB_PASSWORD);
+$db = new Db();
 
 /**
  * Users
@@ -44,23 +44,14 @@ $users = new Users();
 
 /**
  * We don't need this data if this is system request
- * Pages manipulation
- * IP management - IP ban
- * Statistics
- * Referer informations
  */
 if (!strstr($_SERVER['PHP_SELF'], '/cronjob/')) {
-    require_once BASEDIR . "include/pages.php";
-    require_once BASEDIR . "include/antidos.php";
-    require_once BASEDIR . "include/counters.php";
-    require_once BASEDIR . "include/referer.php";
+	$current_page = new Page();
+    new Manageip();
+    new Counter();
+    new Referer();
 
-    /**
-     * Show website maintenance page
-     */
-    if ($vavok->get_configuration('siteOff') == 1 && !strstr($_SERVER['PHP_SELF'], 'pages/maintenance.php') && !strstr($_SERVER['PHP_SELF'], 'pages/input.php') && !$users->is_administrator() && !strstr($_SERVER['PHP_SELF'], 'pages/login.php')) {
-        $vavok->redirect_to(website_home_address() . "/pages/maintenance.php");
-    }
+    require_once BASEDIR . "include/pages.php";
 }
 
 
