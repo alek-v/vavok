5ff823db7512397228f0b890ef06efd1fbb0147f
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2020-09-30 17:10:53 +0200

    Updated admin list page

--- adminpanel/adminlist.php
+++ adminpanel/adminlist.php
@@ -8,31 +8,15 @@ require_once '../include/startup.php';
 
 if (!$vavok->go('users')->is_reg() || !$vavok->go('users')->check_permissions(basename(__FILE__))) $vavok->redirect_to("../pages/input.php?action=exit");
 
-if (!empty($_GET['action'])) {
-    $action = $vavok->check($_GET["action"]);
-} else {
-    $action = '';
-} 
-if (!empty($_GET['page'])) {
-    $page = $vavok->check($_GET["page"]);
-} else {
-    $page = '';
-} 
-if (!empty($_GET['view'])) {
-    $view = $vavok->check($_GET["view"]);
-} else {
-    $view = '';
-} 
+$action = isset($_GET['action']) ? $vavok->check($_GET['action']) : '';
+$page = isset($_GET['page']) ? $vavok->check($_GET['page']) : '';
+$view = isset($_GET['view']) ? $vavok->check($_GET['view']) : '';
 
 $vavok->go('current_page')->page_title = $vavok->go('localization')->string('modlist');
-
 $vavok->require_header();
 
- 
-
 if (empty($action)) {
-
-    echo '<img src="../images/img/user.gif" alt=""> <b>' . $vavok->go('localization')->string('adminlistl') . '</b><br><br>'; 
+    echo '<p><img src="../images/img/user.gif" alt=""> <b>' . $vavok->go('localization')->string('adminlistl') . '</b></p>'; 
 
     $num_items = $vavok->go('db')->count_row('vavok_users', "perm='101' OR perm='102' OR perm='103' OR perm='105'");
     $items_per_page = 10;
@@ -47,13 +31,13 @@ if (empty($action)) {
             if ($item['perm'] == '101' or $item['perm'] == '102' or $item['perm'] == '103' or $item['perm'] == '105' or $item['perm'] == '106') {
                 $lnk = "<div class=\"a\"><a href=\"../pages/user.php?uz=" . $item['id'] . "\" class=\"sitelink\">" . $item['name'] . "</a> - " . $vavok->go('users')->user_status($item['perm']) . "</div>";
                 echo $lnk . "<br>";
-            } 
-        } 
-    } 
+            }
+        }
+    }
 
     echo $navigation->get_navigation();
+}
 
-} 
 echo '<p><a href="./" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('admpanel') . '</a><br>';
 echo $vavok->homelink() . '</p>';
 
