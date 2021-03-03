94a253c55d9615d87d2b3be2e8c911ade4adaca3
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-03-03 20:49:33 +0100

    Updated version

--- include/startup.php
+++ include/startup.php
@@ -2,10 +2,11 @@
 /**
  * Author:    Aleksandar Vranešević
  * URI:       https://vavok.net
+ * Updated:   03.03.2021. 20:44:59
  */
 
 define('START_TIME', microtime(true));
-define('VERSION', '1.5.6');
+define('VERSION', '1.5.6.1');
 
 /**
  * Root dir for including system files
