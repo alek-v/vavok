66eedf5e43137d8ebec7f1ce1e5d21d0efe09d0a
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-06-19 01:40:11 +0200

    Updated email text for registration

--- pages/registration.php
+++ pages/registration.php
@@ -74,14 +74,23 @@ if ($action == 'reguser') {
                             $vavok->go('users')->register($log, $password, $vavok->get_configuration('regConfirm'), $registration_key, MY_THEME, $mail); // register user
                              
                             // send email with reg. data
-                            if ($vavok->get_configuration('regConfirm') == '1') {
-                                $needkey = "\r\n" . $vavok->go('localization')->string('emailpart5') . "\r\n" . $vavok->go('localization')->string('yourkey') . ": " . $registration_key . "\r\n" . $vavok->go('localization')->string('emailpart6') . ":\r\n\r\n" . $vavok->website_home_address() . "/pages/key.php?action=inkey&key=" . $registration_key . "\r\n\r\n" . $vavok->go('localization')->string('emailpart7') . "\r\n\r\n";
+                            if ($vavok->get_configuration('regConfirm') == 1) {
+                                $needkey = "<p>" . $vavok->go('localization')->string('emailpart5') . "</p>
+                                <p>" . $vavok->go('localization')->string('yourkey') . ": " . $registration_key . "</p>
+                                <p>" . $vavok->go('localization')->string('emailpart6') . ":</p>
+                                <p>" . $vavok->website_home_address() . "/pages/key.php?action=inkey&key=" . $registration_key . "</p>
+                                <p>" . $vavok->go('localization')->string('emailpart7') . "</p>";
                             } else {
-                                $needkey = "\r\n";
-                            } 
+                                $needkey = "<br />";
+                            }
 
                             $subject = $vavok->go('localization')->string('regonsite') . ' ' . $vavok->get_configuration('title');
-                            $regmail = $vavok->go('localization')->string('hello') . " " . $log . "\r\n\r\n" . $vavok->go('localization')->string('emailpart1') . " " . $vavok->get_configuration('homeUrl') . " \r\n" . $vavok->go('localization')->string('emailpart2') . ":\r\n\r\n" . $vavok->go('localization')->string('username') . ": " . $log . "\r\n" . $needkey . "" . $vavok->go('localization')->string('emailpart3') . "\r\n" . $vavok->go('localization')->string('emailpart4') . "";
+                            $regmail = "<p>" . $vavok->go('localization')->string('hello') . " " . $log . "!</p>
+                            <p>" . $vavok->go('localization')->string('emailpart1') . " " . $vavok->get_configuration('homeUrl') . "</p>
+                            <p>" . $vavok->go('localization')->string('emailpart2') . ":</p>
+                            <p>" . $vavok->go('localization')->string('username') . ": " . $log . "</p>
+                            <p>" . $needkey . "" . $vavok->go('localization')->string('emailpart3') . "</p>
+                            <p>" . $vavok->go('localization')->string('emailpart4') . "</p>";
 
                             // Send confirmation email
                             $newMail = new Mailer;
