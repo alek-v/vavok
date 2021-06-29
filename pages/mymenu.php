bc6c148675a0709936fc18081be1dcb699026bd5
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-06-29 23:12:38 +0200

    Use sitelink's

--- pages/mymenu.php
+++ pages/mymenu.php
@@ -2,7 +2,6 @@
 /**
  * Author:    Aleksandar Vranešević
  * URI:       https://vavok.net
- * Updated:   04.09.2020. 23:12:21
  */
 
 require_once '../include/startup.php';
@@ -10,19 +9,17 @@ require_once '../include/startup.php';
 $vavok->require_header();
 
 if ($vavok->go('users')->is_reg()) {
-	echo '
-	<a href="' . BASEDIR . 'pages/inbox.php" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('inbox') . ' (' . $vavok->go('users')->user_mail($vavok->go('users')->user_id) . ')</a><br>
-	<a href="' . BASEDIR . 'pages/ignor.php" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('ignorlist') . '</a><br>
-	<a href="' . BASEDIR . 'pages/buddy.php" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('contacts') . '</a><br>
-	<a href="' . BASEDIR . 'pages/profile.php" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('updprof') . '</a><br>
-	<a href="' . BASEDIR . 'pages/settings.php" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('settings') . '</a><br> 
-	<a href="' . BASEDIR . 'pages/input.php?action=exit" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('logout') . '</a><br>
-	';
+	echo $vavok->sitelink(HOMEDIR . 'pages/inbox.php', $vavok->go('localization')->string('inbox') . ' (' . $vavok->go('users')->user_mail($vavok->go('users')->user_id) . ')', '', '<br />') . 
+	$vavok->sitelink(HOMEDIR . 'pages/ignor.php', $vavok->go('localization')->string('ignorlist'), '', '<br />') .
+	$vavok->sitelink(HOMEDIR . 'pages/buddy.php', $vavok->go('localization')->string('contacts'), '', '<br />') .
+	$vavok->sitelink(HOMEDIR . 'pages/profile.php', $vavok->go('localization')->string('updprof'), '', '<br />') .
+	$vavok->sitelink(HOMEDIR . 'pages/settings.php', $vavok->go('localization')->string('settings'), '', '<br />') .
+	$vavok->sitelink(HOMEDIR . 'pages/input.php?action=exit', $vavok->go('localization')->string('logout'));
 } else {
     echo '<p>' . $vavok->go('localization')->string('notloged') . '</p>';
-} 
+}
 
-$vavok->homelink('<p>', '</p>');
+echo $vavok->homelink('<p>', '</p>');
 
 $vavok->require_footer();
 
