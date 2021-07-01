a61d06f9053337a955ff7d46c1a77876cb0d7361
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-07-01 20:31:20 +0200

    Added database prefix

--- cronjob/clean-email-tokens.php
+++ cronjob/clean-email-tokens.php
@@ -5,6 +5,6 @@ require_once '../include/startup.php';
 $now = new DateTime();
 $new_time = $now->format('Y-m-d H:i:s');
 
-$vavok->go('db')->delete(DB_PREFIX . 'email_confirm', "expiration_time < '{$new_time}'");
+$vavok->go('db')->delete(DB_PREFIX . 'tokens', "expiration_time < '{$new_time}'");
 
 ?>
