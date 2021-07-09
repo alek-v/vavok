d8f0d86c65d6601397aef9ea3b78989d70ea0744
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-07-09 21:34:50 +0200

    Localization updated

--- include/lang/english/mail.php
+++ include/lang/english/mail.php
@@ -2,20 +2,19 @@
 // (c) vavok.net
 
 $lang_mail = array();
-$lang_mail['howtolostpass'] = "Write your username and email.<br />Your password will be sent to you in an email";
-$lang_mail['newpassfromsite'] = "New paassword from site";
+$lang_mail['howtolostpass'] = "Write your username and email.<br />Password will be sent to you in an email";
+$lang_mail['newpassfromsite'] = "New login details for site";
 $lang_mail['hello'] = "Hello";
-$lang_mail['yournewdata'] = "Your new login information needed to access the site";
+$lang_mail['yournewdata'] = "Your new login information required to access the site";
 $lang_mail['lnkforautolog'] = "Link for autologin to the site";
-$lang_mail['ycchngpass'] = "You can change your password in your profile page";
-$lang_mail['passgen'] = "Password succesfully generated!<br />New password has been sent to email address you set up in your profile";
+$lang_mail['ycchngpass'] = "You can change your password in your profile page after login";
+$lang_mail['passgen'] = "Password succesfully generated!<br />New password has been sent to the email address you set up in your profile";
 $lang_mail['wrongcaptcha'] = "Captcha solution incorrect";
 $lang_mail['wrongmail'] = "You have written wrong email address";
 $lang_mail['noneededdata'] = "You didn't complete all of the required data ";
 $lang_mail['delsubok'] = "You have been successfully unsubscribed from site news"; // site news subscription
 $lang_mail['unsubcodefail'] = "Unsubscription code invalid";
 $lang_mail['unsubfail'] = "Unsubscribing failed";
-
 
 $lang_home = array_merge($lang_home, $lang_mail);
 ?>
