8a3a1192aedd63cdc56d25ac631d87eb86ba5a8d
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-07-09 21:35:38 +0200

    Implemented handling of POST and GET data

--- mail/lostpassword.php
+++ mail/lostpassword.php
@@ -6,11 +6,6 @@
 
 require_once '../include/startup.php';
 
-$page = isset($_GET['page']) ? $vavok->check($_GET['page']) : '';
-$logus = isset($_POST['logus']) ? $vavok->check($_POST['logus']) : '';
-$mailsus = isset($_POST['mailsus']) ? $vavok->check($_POST['mailsus']) : '';
-$captcha = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : ''; // Captcha code
-
 // Page settings
 $vavok->go('current_page')->page_title = $vavok->go('localization')->string('lostpass');
 
@@ -21,7 +16,7 @@ $vavok->go('current_page')->append_head_tags('<script src="https://www.google.co
 
 $vavok->require_header();
 
-if (empty($page) || $page == 'index') {
+if (empty($vavok->post_and_get('page')) || $vavok->post_and_get('page') == 'index') {
 	$this_page = new PageGen('pages/registration/lost_password.tpl');
 
     // Show reCAPTCHA
@@ -31,24 +26,30 @@ if (empty($page) || $page == 'index') {
 }
 
 // Send mail
-if ($page == 'send') {
-    if (!empty($logus) && !empty($mailsus)) {
-        $userx_id = $vavok->go('users')->getidfromnick($logus);
+if ($vavok->post_and_get('page') == 'send') {
+    if (!empty($vavok->post_and_get('logus')) && !empty($vavok->post_and_get('mailsus'))) {
+        $userx_id = $vavok->go('users')->getidfromnick($vavok->post_and_get('logus'));
         $show_userx = $vavok->go('db')->get_data('vavok_about', "uid='" . $userx_id . "'", 'email');
 
         $checkmail = trim($show_userx['email']);
 
-        if ($mailsus == $checkmail) {
-			if ($vavok->recaptcha_response($captcha)['success'] == true) {
+        if ($vavok->post_and_get('mailsus') == $checkmail) {
+			if ($vavok->recaptcha_response($vavok->post_and_get('g-recaptcha-response'))['success'] == true) {
 
-                $newpas = generate_password();
+                $newpas = $vavok->generate_password();
                 $new = $vavok->go('users')->password_encrypt($newpas);
 
                 $subject = $vavok->go('localization')->string('newpassfromsite') . ' ' . $vavok->get_configuration('title');
-                $mail = $vavok->go('localization')->string('hello') . " " . $logus . "\r\n" . $vavok->go('localization')->string('yournewdata') . " " . $vavok->get_configuration('homeUrl') . "\r\n" . $vavok->go('localization')->string('username') . ": " . $logus . "\r\n" . $vavok->go('localization')->string('pass') . ": " . $newpas . "\r\n\r\n" . $vavok->go('localization')->string('lnkforautolog') . ":\r\n" . $vavok->get_configuration('homeUrl') . "/pages/input.php?log=" . $logus . "&pass=" . $newpas . "&cookietrue=1\r\n" . $vavok->go('localization')->string('ycchngpass')  . "\r\n";
+                $mail = $vavok->go('localization')->string('hello') . " " . $vavok->post_and_get('logus') . "<br /><br />
+                " . $vavok->go('localization')->string('yournewdata') . " " . $vavok->get_configuration('homeUrl') . "<br /><br />
+                " . $vavok->go('localization')->string('username') . ": " . $vavok->post_and_get('logus') . "<br />
+                " . $vavok->go('localization')->string('pass') . ": " . $newpas . "<br /><br />
+                " . $vavok->go('localization')->string('lnkforautolog') . ":<br />
+                " . $vavok->get_configuration('homeUrl') . "/pages/input.php?log=" . $vavok->post_and_get('logus') . "&pass=" . $newpas . "&cookietrue=1<br /><br />
+                " . $vavok->go('localization')->string('ycchngpass');
 
 				$send_mail = new Mailer();
-				$send_mail->queue_email($mailsus, $subject, $mail);
+				$send_mail->queue_email($vavok->post_and_get('mailsus'), $subject, $mail);
 
                 // Update user's profile
                 $vavok->go('db')->update('vavok_users', 'pass', $new, "id='{$userx_id}'");
--- mail/subdel.php
+++ mail/subdel.php
@@ -2,25 +2,21 @@
 /**
  * Author:    Aleksandar Vranešević
  * URI:       https://vavok.net
- * Updated:   04.09.2020. 23:17:50
  */
 
 require_once '../include/startup.php';
 $vavok->require_header();
 
-$code = !empty($_GET['subdel']) ? $vavok->check($_GET['subdel']) : '';
-$subscriptionName = !empty($_GET['sn']) ? $vavok->check($_GET['sn']) : '';
-
-if (!empty($code)) {
-    if (preg_match("/[^a-z0-9]/", $code)) {
-        $email_check = $vavok->go('db')->get_data('subs', "user_pass='" . $code . "'");
-        if ($code == $email_check['user_pass'] && (empty($email_check['subscripton_name']) || $email_check['subscripton_name'] == $subscriptionName)) {
-            if ($email_check['user_id'] > 0 && ($subscriptionName == 'sitenews' || empty($subscriptionName))) {
+if (!empty($vavok->post_and_get('subdel'))) {
+    if (preg_match("/[^a-z0-9]/", $vavok->post_and_get('subdel'))) {
+        $email_check = $vavok->go('db')->get_data('subs', "user_pass='" . $vavok->post_and_get('subdel') . "'");
+        if ($vavok->post_and_get('subdel') == $email_check['user_pass'] && (empty($email_check['subscripton_name']) || $email_check['subscripton_name'] == $vavok->post_and_get('sn'))) {
+            if ($email_check['user_id'] > 0 && ($vavok->post_and_get('sn') == 'sitenews' || empty($vavok->post_and_get('sn')))) {
                 $uz_log = $vavok->go('users')->getnickfromid($email_check['user_id']);
                 $vavok->go('db')->update('vavok_profil', array('subscri', 'newscod'), array(0, ''), "uid='" . $email_check['user_id'] . "'");
             } 
 
-            $vavok->go('db')->delete('subs', "user_pass='{$code}'");
+            $vavok->go('db')->delete('subs', "user_pass='{$vavok->post_and_get('subdel')}'");
 
             echo '<p>' . $vavok->go('localization')->string('hello') . ' ' . $uz_log . '!</p>
             <p>' . $vavok->go('localization')->string('delsubok') . '!</p>';
