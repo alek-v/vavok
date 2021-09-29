6dbc2e4d177bd4153751b845fd74e31a3e82130d
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-09-29 19:59:39 +0200

    Navigation improvements

--- adminpanel/adminchat.php
+++ adminpanel/adminchat.php
@@ -6,11 +6,7 @@
 
 require_once '../include/startup.php';
 
-if (!$vavok->go('users')->is_reg() || !$vavok->go('users')->check_permissions(basename(__FILE__))) $vavok->redirect_to("../pages/input.php?action=exit");
-
-$rand = rand(100, 999);
-$dates = date("d.m.y");
-$times = date("H:i");
+if (!$vavok->go('users')->is_reg() || !$vavok->go('users')->check_permissions(basename(__FILE__))) $vavok->redirect_to('../?auth_error');
 
 $vavok->go('current_page')->page_title = $vavok->go('localization')->string('adminchat');
 
@@ -20,7 +16,7 @@ echo '<img src="../images/img/menu.gif" alt=""> <b>' . $vavok->go('localization'
 
 if (empty($vavok->post_and_get('action'))) {
     echo '<a href="#down"><img src="../images/img/downs.gif" alt=""></a> ';
-    echo '<a href="adminchat.php?r=' . $rand . '" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('refresh') . '</a><br>';
+    echo '<a href="adminchat.php?r=' . rand(100, 999) . '" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('refresh') . '</a><br>';
 
     echo'<hr><form action="process.php?action=acadd" method="post"><b>' . $vavok->go('localization')->string('message') . '</b><br>';
     echo'<textarea cols="80" rows="5" name="msg"></textarea><br>';
@@ -30,75 +26,40 @@ if (empty($vavok->post_and_get('action'))) {
     $file = $vavok->get_data_file('adminchat.dat');
     $file = array_reverse($file);
     $total = count($file);
+
     if ($total < 1) {
         echo'<br><img src="../images/img/reload.gif" alt=""> <b>' . $vavok->go('localization')->string('nomsgs') . '</b><br>';
-    } 
-    if (empty($_GET['start'])) $start = 0;
-    else $start = $_GET['start'];
-    if ($total < $start + 10) {
+    }
+
+    $navigation = new Navigation(10, $total, $vavok->post_and_get('page'), 'adminchat.php?'); // start navigation
+
+    $limit_start = $navigation->start()['start']; // starting point
+
+    if ($total < $limit_start + 10) {
         $end = $total;
     } else {
-        $end = $start + 10;
-    } 
-    for ($i = $start; $i < $end; $i++) {
+        $end = $limit_start + 10;
+    }
+
+    for ($i = $limit_start; $i < $end; $i++) {
         $data = explode("|", $file[$i]); 
-        // ////////////////////////////////////////////////////////////
+
         $statwho = $vavok->go('users')->user_online($data[1]); 
-        // /////////////////////////////////////////////////////////////
+
         $data_text = $vavok->getbbcode($data[0]);
 
-        echo'<div class=b><b><a href="../pages/user.php?uz=' . $data[1] . '" class="btn btn-outline-primary sitelink"> ' . $data[1] . ' </a></b> ' . $statwho;
+        echo '<div class=b><b><a href="../pages/user.php?uz=' . $data[1] . '" class="btn btn-outline-primary sitelink"> ' . $data[1] . ' </a></b> ' . $statwho;
 
-        if ($dates == $data[2]) {
+        if (date('d.m.y') == $data[2]) {
             $data[2] = '<font color="#FF0000">' . $vavok->go('localization')->string('today') . '</font>';
         }
 
         echo'<small> (' . $data[2] . ' / ' . $data[3] . ')</small></div>' . $data_text . '<br><small><font color="#CC00CC">[' . $data[4] . ', ' . $data[5] . ']</font></small>';
         echo'<br>';
-    } 
+    }
 
     echo '<hr>';
-    if ($start != 0) {
-        echo '<a href="adminchat.php?start=' . ($start - 10) . '" class="btn btn-outline-primary sitelink">&lt; ' . $vavok->go('localization')->string('back') . '</a> ';
-    } else {
-        echo'&lt; ' . $vavok->go('localization')->string('back') . '';
-    } 
-    echo '|';
-    if ($total > $start + 10) {
-        echo ' <a href="adminchat.php?start=' . ($start + 10) . '" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('forw') . ' &gt;</a>';
-    } else {
-        echo'' . $vavok->go('localization')->string('forw') . ' &gt;';
-    }
-
-    if ($total > 0) {
-        $ba = ceil($total / 10);
-        $ba2 = $ba * 10 - 10;
-
-        echo '<br><hr>Page:';
-        $asd = $start - (10 * 3);
-        $asd2 = $start + (10 * 4);
-
-        if ($asd < $total && $asd > 0) {
-            echo ' <a href="adminchat.php?start=0" class="btn btn-outline-primary sitelink">1</a> ... ';
-        } 
-
-        for($i = $asd; $i < $asd2;) {
-            if ($i < $total && $i >= 0) {
-                $ii = floor(1 + $i / 10);
-
-                if ($start == $i) {
-                    echo ' <b>(' . $ii . ')</b>';
-                } else {
-                    echo ' <a href="adminchat.php?start=' . $i . '" class="btn btn-outline-primary sitelink">' . $ii . '</a>';
-                } 
-            } 
-
-            $i = $i + 10;
-        } 
-        if ($asd2 < $total) {
-            echo ' ... <a href="adminchat.php?start=' . $ba2 . '" class="btn btn-outline-primary sitelink">' . $ba . '</a>';
-        }
-    }
+    echo $navigation->get_navigation();
 
     echo '<br><br>';
 
@@ -114,7 +75,7 @@ if ($vavok->post_and_get('action') == 'prodel') {
 
 if (isset($total) && $total > 0 && ($_SESSION['permissions'] == 101 || $_SESSION['permissions'] == 102)) {
     echo '<br><a href="adminchat.php?action=prodel" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('cleanchat') . '</a>';
-} 
+}
 
 echo '<p><a href="./" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('admpanel') . '</a><br>';
 echo $vavok->homelink() . '</p>';
--- adminpanel/ban.php
+++ adminpanel/ban.php
@@ -6,87 +6,72 @@
 
 require_once '../include/startup.php';
 
-if ($vavok->go('users')->is_reg()) {
-    if ($_SESSION['permissions'] == 101 || $_SESSION['permissions'] == 102) {
+if (!$vavok->go('users')->is_reg() || ($_SESSION['permissions'] != 101 && $_SESSION['permissions'] != 102)) { $vavok->redirect_to('../?auth_error'); }
 
-        $vavok->go('current_page')->page_title = "IP ban";
-        $vavok->require_header();
+$vavok->go('current_page')->page_title = 'IP ban';
+$vavok->require_header();
 
-        echo '<img src="../images/img/menu.gif" alt=""> <b>IP ban panel</b><br><br>';
+echo '<p><img src="../images/img/menu.gif" alt="" /> <b>IP ban panel</b></p>';
 
-        if (empty($vavok->post_and_get('action'))) {
-            $file = $vavok->get_data_file('ban.dat');
-            $total = count($file);
-            if (empty($_GET['start'])) $start = 0;
-            else $start = $_GET['start'];
-            if ($total < $start + 10) {
-                $end = $total;
-            } else {
-                $end = $start + 10;
-            } 
-            for ($i = $start; $i < $end; $i++) {
-                $file = $vavok->get_data_file('ban.dat');
-                $file = array_reverse($file);
-                $data = explode("|", $file[$i]);
-                $i2 = round($i + 1);
+if (empty($vavok->post_and_get('action'))) {
+    $file = $vavok->get_data_file('ban.dat');
+    $total = count($file);
 
-                $num = $total - $i-1;
+    $navigation = new Navigation(10, $total, $vavok->post_and_get('page'), 'ban.php?'); // start navigation
 
-                echo $i2 . '. ' . $data[1] . ' <br><a href="process.php?action=razban&amp;start=' . $start . '&amp;id=' . $num . '" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('delban') . '</a><hr>';
-            } 
+    $limit_start = $navigation->start()['start']; // starting point
 
-            if ($total < 1) {
-                echo'<br><img src="../images/img/reload.gif" alt="" /> ' . $vavok->go('localization')->string('emptylist') . '<br><br>';
-            } 
-
-            if ($start != 0) {
-                echo '<a href="ban.php?start=' . ($start - 10) . '" class="btn btn-outline-primary sitelink">&lt; ' . $vavok->go('localization')->string('back') . '</a> ';
-            } else {
-                echo'&lt; ' . $vavok->go('localization')->string('back') . ' ';
-            } 
-            echo'|';
-            if ($total > $start + 10) {
-                echo ' <a href="ban.php?start=' . ($start + 10) . '" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('forw') . ' &gt;</a>';
-            } else {
-                echo' ' . $vavok->go('localization')->string('forw') . ' &gt;';
-            }
-
-            echo '<hr>';
-
-            $form = new PageGen('forms/form.tpl');
-            $form->set('form_method', 'post');
-            $form->set('form_action', 'process.php?action=zaban&amp;start=' . $start);
-
-            $input = new PageGen('forms/input.tpl');
-            $input->set('label_for', 'ips');
-            $input->set('label_value', $vavok->go('localization')->string('iptoblock'));
-            $input->set('input_name', 'ips');
-            $input->set('input_id', 'ips');
-
-            $form->set('website_language[save]', $vavok->go('localization')->string('confirm'));
-            $form->set('fields', $input->output());
-            echo $form->output();
-
-            echo '<hr>';
-
-            echo '<p>' . $vavok->go('localization')->string('ipbanexam') . '</p>';
-            echo '<p>' . $vavok->go('localization')->string('allbanips') . ': ' . $total . '</p>';
-
-            if ($total > 1) {
-                echo'<p><a href="process.php?action=delallip" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('dellist') . '</a></p>';
-            }
-        }
-
-        echo '<p><a href="./" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('admpanel') . '</a><br>';
-        echo $vavok->homelink() . '</p>';
+    if ($total < $limit_start + 10) {
+        $end = $total;
     } else {
-        header ("Location: ../index.php?error");
-        exit;
+        $end = $limit_start + 10;
+    }
+
+    for ($i = $limit_start; $i < $end; $i++) {
+        $file = $vavok->get_data_file('ban.dat');
+        $file = array_reverse($file);
+        $data = explode("|", $file[$i]);
+        $i2 = round($i + 1);
+
+        $num = $total - $i-1;
+
+        echo $i2 . '. ' . $data[1] . ' <br><a href="process.php?action=razban&amp;id=' . $num . '" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('delban') . '</a><hr>';
     } 
-} else {
-    header ("Location: ../index.php?error");
-    exit;
-} 
+
+    if ($total < 1) {
+        echo '<p><img src="../images/img/reload.gif" alt="" /> ' . $vavok->go('localization')->string('emptylist') . '</p>';
+    } 
+
+    echo $navigation->get_navigation();
+
+    echo '<hr>';
+
+    $form = new PageGen('forms/form.tpl');
+    $form->set('form_method', 'post');
+    $form->set('form_action', 'process.php?action=zaban&amp;start=' . $start);
+
+    $input = new PageGen('forms/input.tpl');
+    $input->set('label_for', 'ips');
+    $input->set('label_value', $vavok->go('localization')->string('iptoblock'));
+    $input->set('input_name', 'ips');
+    $input->set('input_id', 'ips');
+
+    $form->set('website_language[save]', $vavok->go('localization')->string('confirm'));
+    $form->set('fields', $input->output());
+    echo $form->output();
+
+    echo '<hr>';
+
+    echo '<p>' . $vavok->go('localization')->string('ipbanexam') . '</p>';
+    echo '<p>' . $vavok->go('localization')->string('allbanips') . ': ' . $total . '</p>';
+
+    if ($total > 1) {
+        echo '<p><a href="process.php?action=delallip" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('dellist') . '</a></p>';
+    }
+}
+
+echo '<p><a href="./" class="btn btn-outline-primary sitelink">' . $vavok->go('localization')->string('admpanel') . '</a><br>';
+echo $vavok->homelink() . '</p>';
 
 $vavok->require_footer();
 
