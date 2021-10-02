e78c9539ec2b49f37c066f7422c899ab28036003
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-10-03 01:03:18 +0200

    Added composer third party code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.5.1',
+      'version' => '6.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
+    ),
+    'tinymce/tinymce' => 
+    array (
+      'pretty_version' => '5.9.2',
+      'version' => '5.9.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '48c665ad12ba0e4d8068ba0784026c7488aa4746',
+    ),
+  ),
+);
