efaedc335b952216d03a844ec4873b3546326ab2
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2021-10-10 20:56:15 +0200

    Update userlist.php

--- pages/userlist.php
+++ pages/userlist.php
@@ -6,29 +6,20 @@
 
 require_once '../include/startup.php';
 
-$view = !empty($vavok->post_and_get('view')) ? $vavok->post_and_get('view') : 'name';
-
 $num_items = $vavok->go('users')->regmemcount(); // no. reg. members
 $items_per_page = 10;
 
-$navigation = new Navigation($items_per_page, $num_items, $vavok->post_and_get('page'), 'userlist.php?'); // start navigation
+// Start navigation
+$navigation = new Navigation($items_per_page, $num_items, $vavok->post_and_get('page'), 'userlist.php?');
 
-$limit_start = $navigation->start()['start']; // starting point
-
-// changable sql
-if ($vavok->post_and_get('view') == 'name') {
-    $sql = "SELECT id, name FROM vavok_users ORDER BY name LIMIT $limit_start, $items_per_page";
-} else {
-    $sql = "SELECT id, name FROM vavok_users ORDER BY id DESC LIMIT $limit_start, $items_per_page";
-}
+// Starting point
+$limit_start = $navigation->start()['start'];
 
 $vavok->go('current_page')->page_title = $vavok->go('localization')->string('userlist');
 $vavok->require_header();
 
 if ($num_items > 0) {
-    foreach ($vavok->go('db')->query($sql) as $item) {
-        if ($item['id'] == 0 || $item['name'] == 'System') continue;
-
+    foreach ($vavok->go('db')->query("SELECT id, name FROM " . DB_PREFIX . "vavok_users ORDER BY name LIMIT $limit_start, $items_per_page") as $item) {
         echo '<div class="a">';
         echo '<a href="../pages/user.php?uz=' . $item['id'] . '">' . $item['name'] . '</a> - joined: ' . $vavok->date_fixed($vavok->go('users')->user_info('regdate', $item['id']), 'd.m.Y.'); // update lang
         echo '</div>';
