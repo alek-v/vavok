0299f9baf4322a4759779da1b2dd1d9158ab3c36
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2022-07-21 20:18:13 +0200

    Fixed generatePassword() method

--- app/classes/Core.php
+++ app/classes/Core.php
@@ -553,15 +553,21 @@ class Core {
 	    return $file;
 	}
 
-	// generate password
+	/**
+	 * Generate password
+	 * 
+	 * @return string
+	 */
 	public function generatePassword() {
 	    $length = rand(10, 12);
 	    $salt = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789";
 	    $len = strlen($salt);
 	    $makepass = '';
-	    mt_srand(10000000 * (double)microtime());
+		mt_srand(crc32(microtime())); 
+
 	    for ($i = 0; $i < $length; $i++)
 	    $makepass .= $salt[mt_rand(0, $len - 1)];
+
 	    return $makepass;
 	} 
 
