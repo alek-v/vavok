fcf39704081625af14debf2321354dce9bf21237
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2022-12-24 15:06:01 +0100

    feat: Add option to show blog posts in localization from the URL parameter

--- app/models/BlogModel.php
+++ app/models/BlogModel.php
@@ -9,15 +9,13 @@ use App\Classes\Navigation;
 use App\Classes\BlogComments;
 
 class BlogModel extends BaseModel {
-    protected $page_name;             // Page name
-    protected $page_language;         // Page language
-    protected $page_title;            // Title
-    protected $page_content;          // Content
-    protected $published;             // Visitors can see page
-    protected $page_author;           // Page author
-    protected $page_created_date;     // Page created date
-    protected $head_tags;             // Head tags
-    protected $page_published_date;   // Date when post is published
+    private string $page_title;            // Title
+    private string $page_content;          // Content
+    private int $published;                // Visitors can see page
+    private string $page_author;           // Page author
+    private string $page_created_date;     // Page created date
+    private string $head_tags;             // Head tags
+    private string $page_published_date;   // Date when post is published
 
     /**
      * Blog
@@ -25,16 +23,19 @@ class BlogModel extends BaseModel {
      * @param array $params
      * @return array
      */
-    public function index($params = [])
+    public function index(array $params = []): array
     {
         // Content of the page
         $this_page['content'] = '';
 
-        // Blog category
-        $pg = isset($params[0]) && $params[0] !== 'category' ? $params[0] : ''; // blog page
+        // Blog page
+        $pg = isset($params[0]) && $params[0] !== 'category' && strlen($params[0]) != 2 ? $params[0] : '';
 
-        $items_per_page = 9; // How many blog posts to show per page
-        $comments_per_page = 0; // How many comments to show per page
+        // Blog posts to show per page
+        $items_per_page = 9;
+
+        // Comments to show per page, 0 = unlimited
+        $comments_per_page = 0;
 
         switch ($pg) {
             case isset($pg):
@@ -45,9 +46,9 @@ class BlogModel extends BaseModel {
                 if (!empty($blog_page_data) && is_array($blog_page_data)) $this_page = array_merge($blog_page_data, $this_page);
 
                 // Redirect to blog main page if page does not exist
-                $page_id = !empty($this->page_id) or $this->redirection(HOMEDIR . 'blog/');
+                !empty($this->page_id) or $this->redirection(HOMEDIR . 'blog/');
 
-                // Update user's localization when page's language is different then current localization
+                // Update user's localization when page's language is different from current localization
                 $this->user->updatePageLocalization($this_page['lang']);
 
                 // Generate page
@@ -57,10 +58,10 @@ class BlogModel extends BaseModel {
                 // Author link
                 $author_full_name = $this->user->userInfo('full_name', $this->page_author);
                 $author_name = !empty($author_full_name) ? $author_full_name : $this->user->getNickFromId($this->page_author);
-        
+
                 $author_link = '<a href="' . HOMEDIR . 'users/u/' . $this->page_author . '">' . $author_name . '</a>';
                 $post->set('author_link', $author_link);
-        
+
                 // Time created
                 $post->set('created_date', $this->correctDate($this->page_created_date, 'd.m.Y.'));
 
@@ -71,13 +72,13 @@ class BlogModel extends BaseModel {
                 } else {
                     $post->set('published_date', $this->correctDate(time(), 'd.m.Y.'));
                 }
-        
+
                 // Date updated
                 $post->set('date_updated', $this->correctDate($this->page_updated_date, 'd.m.Y.'));
-        
+
                 // Content
                 $post->set('content', $this->page_content);
-        
+
                 // Day and month when post is created
                 $post->set('date-created-day', date('d', $this->page_created_date));
                 $post->set('date-created-month', mb_substr($this->localization->showAll()['ln_all_month'][date('n', $this->page_created_date) - 1], 0, 3));
@@ -91,7 +92,7 @@ class BlogModel extends BaseModel {
                     $post->set('date-published-day', date('d', time()));
                     $post->set('date-published-month', mb_substr($this->localization->showAll()['ln_all_month'][date('n', time()) - 1], 0, 3));
                 }
-        
+
                 // Page URL
                 $post->set('page-link', $this->cleanPageUrl());
 
@@ -106,32 +107,32 @@ class BlogModel extends BaseModel {
 
                 // Start navigation
                 $navi = new Navigation($items_per_page, $total_comments);
-        
+
                 $all_comments = $comments->loadComments($this->page_id, $navi->start()['start'], $comments_per_page);
 
                 // merge blog comments and output from object
                 $merge_all = $post->merge($all_comments);
-        
+
                 $show_comments = $this->container['parse_page'];
                 $show_comments->load('blog/all_comments');
-        
+
                 if ($this->user->userAuthenticated()) {
                     $add_comment = $this->container['parse_page'];
                     $add_comment->load('blog/add_comment');
                     $add_comment->set('add_comment_page', HOMEDIR . 'blog/save_comment/?pid=' . $this->page_id . '&ptl=' . CLEAN_REQUEST_URI);
-        
+
                     $post->set('add_comment', $add_comment->output());
                 }
-        
+
                 $show_comments->set('all_comments', $merge_all);
-        
+
                 $post->set('comments', $show_comments->output());
-        
+
                 // Page tags
                 $sql = $this->db->query("SELECT * FROM tags WHERE page_id='{$this->page_id}' ORDER BY id ASC");
-        
+
                 $tags = '';
-        
+
                 foreach ($sql as $key => $value) {
                     $tag_link = $this->container['parse_page'];
                     $tag_link->load('blog/tag_link');
@@ -139,7 +140,7 @@ class BlogModel extends BaseModel {
                     $tag_link->set('tag_link_name', str_replace('_', ' ', $value['tag_name']));
                     $tags .= $tag_link->output();
                 }
-        
+
                 $post->set('tags', $tags);
 
                 // Page content
@@ -147,24 +148,22 @@ class BlogModel extends BaseModel {
 
                 return $this_page;
 
-                break;
-            
             default:
                 $this_category = isset($params[0]) && isset($params[1]) && $params[0] == 'category' ? $params[1] : '';
-
-                // Add meta tags
-                $this_page['headt'] = '<meta name="robots" content="noindex, follow" />' . "\r\n";
 
                 // Page title
                 $this_page['tname'] = 'Blog';
 
                 // Load index template
                 $show_page = $this->container['parse_page'];
-                $show_page->load("blog/index");
+                $show_page->load('blog/index');
 
                 // User's localization short code
                 // We will use it to get pages with this localization
-                $user_localization_short = $this->user->getPreferredLanguage($this->user->user_data['language'], 'short');
+                $user_localization_short = isset($params[0]) && strlen($params[0]) == 2 ? $params[0] : $this->user->getPreferredLanguage($this->user->user_data['language'], 'short');
+
+                // Update user's localization when page's language is different from current localization
+                $this->user->updatePageLocalization($user_localization_short);
 
                 // Count total posts
                 $total_posts = empty($this_category) ? $this->db->countRow('pages', "type='post' AND published = '2' AND (lang = '{$user_localization_short}' OR lang='')") : $this->db->countRow('tags', "tag_name='{$this_category}'");
@@ -175,7 +174,6 @@ class BlogModel extends BaseModel {
                     $this_page['content'] .= $this->sitelink( HOMEDIR . 'blog/', $this->localization->string('backtoblog'), '<p>', '</p>');
 
                     return $this_page;
-                    break;
                 }
 
                 // start navigation
@@ -209,46 +207,43 @@ class BlogModel extends BaseModel {
                     // load template
                     $page_posts = $this->container['parse_page'];
                     $page_posts->load('blog/blog_post');
-        
+
                     // Linked title
                     $page_posts->set('post_name', '<a href="' . HOMEDIR . 'blog/' . $key['pname'] . '">' . $key['tname'] . '</a>');
-        
+
                     // Replace html headings, images and other tags from content
                     $content = !empty($key['content']) ? strip_tags($this->erase_img(preg_replace('#<h([1-6])>(.*?)<\/h[1-6]>#si', '', $key['content']))) : '';
-        
+
                     // When there is more then 45 words show only first 45 words
                     if (count(explode(' ', $content)) > 45) $content = implode(' ', array_slice(explode(' ', str_replace('<br />', ' ', $content)), 0, 45)) . '...';
-        
+
                     // Day when article is created
                     $page_posts->set('date-created-day', date('d', $key['created']));
-        
+
                     // Month when article is created
                     $page_posts->set('date-created-month', $this->localization->showAll()['ln_all_month'][date('n', $key['created']) - 1]);
-        
+
                     // Day when article is published
                     $page_posts->set('date-published-day', date('d', $key['pubdate']));
-        
+
                     // Month when article is published
                     $page_posts->set('date-published-month', $this->localization->showAll()['ln_all_month'][date('n', $key['pubdate']) - 1]);
-        
+
                     // Year when article is published
                     $page_posts->set('date-published-year', date('Y', $key['pubdate']));
-        
+
                     // Page URL
                     $page_posts->set('page-link', $this->cleanPageUrl());
-        
+
                     // Page text
                     $page_posts->set('post_text', $content);
-        
+
                     // Read more link
                     $page_posts->set('read_more_link', HOMEDIR . 'blog/' . $key['pname']);
-        
-                    // Read more title
-                    $page_posts->set('read_more_title', $this->localization->string('readmore'));
-        
+
                     // Cover image
                     $page_posts->set('page_image', $key['default_img']);
-        
+
                     // blog post objects
                     $all_posts[] = $page_posts;
                 }
@@ -264,8 +259,6 @@ class BlogModel extends BaseModel {
                 $this_page['content'] .= $show_page->output();
         
                 return $this_page;
-
-                break;
         }
     }
 
