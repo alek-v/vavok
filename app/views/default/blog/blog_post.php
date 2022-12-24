bf620327e57a5c246ffaed1262fdef0890b355a7
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2022-12-24 15:01:11 +0100

    refactor: Update localization keys

--- app/views/default/blog/blog_post.php
+++ app/views/default/blog/blog_post.php
@@ -4,7 +4,7 @@
         <div class="card-body">
             <h5 class="card-title">{@post_name}}</h5>
             <p class="card-text mt-4 mb-4">{@post_text}}</p>
-            <p><a href="{@read_more_link}}" class="btn btn-primary">{@read_more_title}}</a></p>
+            <p><a href="{@read_more_link}}" class="btn btn-primary">{@localization[read_more]}}</a></p>
         </div>
         <div class="card-footer">
             <small class="text-muted">{@date-published-day}}. {@date-published-month}} {@date-published-year}}.</small>
